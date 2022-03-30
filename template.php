2c21c78f32de92ffb60aacc79d90eb46006d1bf8
SemBire <semharmebrahtu77@gmail.com>
2022-03-29 19:26:16 -0500

    took out nav bar title

--- template.php
+++ template.php
@@ -22,7 +22,7 @@ echo<<<HERE
     </div>
 
     <nav class="navbar navbar-expand-sm navbar-dark sticky-top"  style="background-color:#FDAC53 ;">
-    <a href="#" class="navbar-brand mb-0 h1" >Easy Recipes</a>
+    
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
     </button>
